98f0e653ac4dc78200a3d13a2075e0c9126e0ac1
ellilglor <thomassmekens14@gmail.com>
2024-08-07 18:02:53 +0200

    Made bot able to log in & setup appsettings.json

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Discord;
+using Discord.WebSocket;
+using Microsoft.Extensions.Configuration;
+
+namespace Kozma.net;
+
+public class Program
+{
+    private static DiscordSocketClient _client;
+
+    public static async Task Main()
+    {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+            .AddUserSecrets<Program>()
+            .Build();
+
+        _client = new DiscordSocketClient();
+        _client.Log += Log;
+
+        await _client.LoginAsync(TokenType.Bot, config.GetValue<string>("botToken"));
+        await _client.StartAsync();
+
+        await Task.Delay(-1);
+    }
+
+    private static Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+}
