139958ae7c9cc89da87804fb5390a96e0551e6e6
ellilglor <thomassmekens14@gmail.com>
2024-08-11 21:15:18 +0200

    Added test command

---
+++ Commands/Server/Test.cs
@@ -0,0 +1,24 @@
+﻿using Discord.Interactions;
+using Discord;
+using Kozma.net.Factories;
+
+namespace Kozma.net.Commands.Server;
+
+public class Test : InteractionModuleBase<SocketInteractionContext>
+{
+    private readonly IEmbedFactory _embedFactory;
+
+    public Test(IEmbedFactory embedFactory)
+    {
+        _embedFactory = embedFactory;
+    }
+
+    [SlashCommand("test", "Kozma's Backpack staff only.")]
+    [RequireUserPermission(GuildPermission.BanMembers | GuildPermission.KickMembers)]
+    public async Task ExecuteAsync()
+    {
+        var embed = _embedFactory.GetAndBuildEmbed("Command used for testing.");
+
+        await ModifyOriginalResponseAsync(msg => msg.Embed = embed);
+    }
+}
