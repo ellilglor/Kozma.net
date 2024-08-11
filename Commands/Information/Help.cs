2f042cdc1d857c916e29c660c23988c32bc48a31
ellilglor <thomassmekens14@gmail.com>
2024-08-11 13:52:31 +0200

    Added bookchance command

--- Commands/Information/Help.cs
+++ Commands/Information/Help.cs
@@ -5,7 +5,7 @@ using Microsoft.Extensions.Configuration;
 
 namespace Kozma.net.Commands.Information;
 
-public class Help : InteractionModuleBase<SocketInteractionContext>, ICommand
+public class Help : InteractionModuleBase<SocketInteractionContext>
 {
     private readonly IEmbedFactory _embedFactory;
     private readonly IConfiguration _config;
@@ -19,22 +19,23 @@ public class Help : InteractionModuleBase<SocketInteractionContext>, ICommand
     [SlashCommand("help", "Explains all commands.")]
     public async Task ExecuteAsync()
     {
+        var inline = false;
         var fields = new List<EmbedFieldBuilder>
         {
-            CreateField("/bookchance", "Get the % chance you have of getting at least 1 Book of Dark Rituals.\n`kats` Amount of Black Kats you encountered."),
-            CreateField("/clear", "Deletes all the messages the bot has sent you."),
-            CreateField("/convert", "Convert your currency. (glorified calculator)\n`amount` Amount you want to convert.\n`rate` Optional custom conversion rate."),
-            CreateField("/findlogs", @"Makes the bot search the database for your item.
+            _embedFactory.CreateField("/bookchance", "Get the % chance you have of getting at least 1 Book of Dark Rituals.\n`kats` Amount of Black Kats you encountered.", inline),
+            _embedFactory.CreateField("/clear", "Deletes all the messages the bot has sent you.", inline),
+            _embedFactory.CreateField("/convert", "Convert your currency. (glorified calculator)\n`amount` Amount you want to convert.\n`rate` Optional custom conversion rate.", inline),
+            _embedFactory.CreateField("/findlogs", @"Makes the bot search the database for your item.
                 `item` Item the bot should look for.\n`months` How far back the bot should search. Default: 6 months.
                 `variants` Check for color variants / item family tree. Default: yes.
                 `clean` Filter out high value uvs. Default: no.
-                `mixed` Check the mixed-trades channel. Default: yes."),
-            CreateField("/lockbox", @"Gives information about a lockbox or tells you what box drops your item.
+                `mixed` Check the mixed-trades channel. Default: yes.", inline),
+            _embedFactory.CreateField("/lockbox", @"Gives information about a lockbox or tells you what box drops your item.
                 `boxes` Get the odds from a lockbox.\n`slime` Find where you can find a special themed box.
-                `item` Find which lockbox drops your item."),
-            CreateField("/punch", "Craft items and roll for Unique Variants without draining your wallet.\n`item` Select the item you want to craft."),
-            CreateField("/rate", "Tells you the crowns per energy rate currently in use."),
-            CreateField("/unbox", "Simulate opening a box and be disappointed for free.\n`box` Select the box you want to open.")
+                `item` Find which lockbox drops your item.", inline),
+            _embedFactory.CreateField("/punch", "Craft items and roll for Unique Variants without draining your wallet.\n`item` Select the item you want to craft.", inline),
+            _embedFactory.CreateField("/rate", "Tells you the crowns per energy rate currently in use.", inline),
+            _embedFactory.CreateField("/unbox", "Simulate opening a box and be disappointed for free.\n`box` Select the box you want to open.", inline)
         };
 
         var embed = _embedFactory.GetEmbed("Here are all my commands:")
@@ -51,10 +52,5 @@ public class Help : InteractionModuleBase<SocketInteractionContext>, ICommand
             msg.Embed = embed;
             msg.Components = components;
         });
-    }
-
-    private static EmbedFieldBuilder CreateField(string name, string value)
-    {
-        return new EmbedFieldBuilder().WithName(name).WithValue(value);
     }
 }
