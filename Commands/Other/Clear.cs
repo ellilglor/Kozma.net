2a8ebd655bcbcc9c47de726618f4922d5bc10bd1
ellilglor <thomassmekens14@gmail.com>
2024-10-18 13:58:25 +0200

    implemented command

---
+++ Commands/Other/Clear.cs
@@ -0,0 +1,31 @@
+﻿using Discord;
+using Discord.Interactions;
+using Kozma.net.Factories;
+
+namespace Kozma.net.Commands.Other;
+
+public class Clear(IEmbedFactory embedFactory) : InteractionModuleBase<SocketInteractionContext>
+{
+    [SlashCommand("clear", "Removes all bot messages in your dms.")]
+    public async Task ExecuteAsync()
+    {
+        var embed = embedFactory.GetAndBuildEmbed("Clearing messages.");
+
+        await ModifyOriginalResponseAsync(msg => {
+            msg.Embed = embed;
+            msg.Components = new ComponentBuilder().Build();
+        });
+
+        var channel = await Context.User.CreateDMChannelAsync();
+        var messages = await channel.GetMessagesAsync(int.MaxValue).FlattenAsync();
+
+        foreach (var msg in messages)
+        {
+            try
+            {
+                if (msg.Author.IsBot) await msg.DeleteAsync();
+            }
+            catch { }
+        }
+    }
+}
