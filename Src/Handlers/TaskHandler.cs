36ebd71c14b6fff718c0f530fcc4e64332917b27
ellilglor <thomassmekens14@gmail.com>
2024-11-21 10:37:08 +0100

    Implemented scam prevention task

--- Src/Handlers/TaskHandler.cs
+++ Src/Handlers/TaskHandler.cs
@@ -1,18 +1,23 @@
 ﻿using Discord;
 using Discord.WebSocket;
+using Kozma.net.Src.Helpers;
 using Kozma.net.Src.Logging;
 using Kozma.net.Src.Services;
 using Microsoft.Extensions.Configuration;
+using System;
 
 namespace Kozma.net.Src.Handlers;
 
-public class TaskHandler(IBot bot, IConfiguration config, IBotLogger logger, IEmbedHandler embedHandler, ITaskService taskService) : ITaskHandler
+public class TaskHandler(IBot bot, IConfiguration config, IBotLogger logger, IEmbedHandler embedHandler, ITaskService taskService, IFileReader jsonFileReader) : ITaskHandler
 {
     private record TaskConfig(double Interval, Func<Task> ExecuteAsync);
+    private record Reminder(string Title, string Description);
 
     private readonly DiscordSocketClient _client = bot.GetClient();
 
     private readonly Dictionary<string, TaskConfig> _tasks = new();
+
+    private static readonly Random _random = new();
 
     public void Initialize()
     {
@@ -42,13 +47,13 @@ public class TaskHandler(IBot bot, IConfiguration config, IBotLogger logger, IEm
         foreach (var task in tasks)
         {
             var taskConfig = _tasks[task.Name];
-            if (task.UpdatedAt.AddHours(taskConfig.Interval) > currentTime) continue;
+            //if (task.UpdatedAt.AddHours(taskConfig.Interval) > currentTime) continue;
 
             await taskConfig.ExecuteAsync();
-            await taskService.UpdateTaskAsync(task.Name);
+            //await taskService.UpdateTaskAsync(task.Name);
         }
 
-        await Task.Delay(TimeSpan.FromMinutes(30));
+        await Task.Delay(TimeSpan.FromMinutes(1));
         await CheckForExpiredTasksAsync();
     }
 
@@ -67,14 +72,24 @@ public class TaskHandler(IBot bot, IConfiguration config, IBotLogger logger, IEm
             .WithDescription("You can edit your posts through the **/tradepostedit** command.\nWe apologise for any inconvenience this may cause.")
             .WithFields(new List<EmbedFieldBuilder>() { embedHandler.CreateField("\u200B", "Interested in what an item has sold for in the past?\nUse the **/findlogs** command.") });
 
-        await wtsChannel.SendMessageAsync(embed: embed.Build());
-        await wtbChannel.SendMessageAsync(embed: embed.Build());
+        /*await wtsChannel.SendMessageAsync(embed: embed.Build());
+        await wtbChannel.SendMessageAsync(embed: embed.Build());*/
     }
 
     private async Task PostScamPreventionAsync()
     {
-        logger.Log(Enums.LogColor.Moderation, "scam");
-        await Task.CompletedTask;
+        if (_client.GetChannel(config.GetValue<ulong>("ids:wtsChannel")) is not SocketTextChannel channel) return;
+        var reminders = await jsonFileReader.ReadAsync<List<Reminder>>(Path.Combine("Data", "Reminders.json"));
+        if (reminders is null) return;
+
+        var reminder = reminders[_random.Next(reminders.Count)];
+        var embed = embedHandler.GetBasicEmbed("Trading Guidelines")
+            .WithFields(new List<EmbedFieldBuilder>() { embedHandler.CreateField(reminder.Title, reminder.Description) })
+            .WithFooter(new EmbedFooterBuilder()
+                    .WithText("Information and communication is essential to negotiations. Please be careful.")
+                    .WithIconUrl(_client.CurrentUser.GetDisplayAvatarUrl()));
+
+        //await channel.SendMessageAsync(embed: embed.Build());
     }
 
     private async Task CheckForNewLogsAsync()
