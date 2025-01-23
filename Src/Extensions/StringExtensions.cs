3d1b0103a76f73862faff859ae7707e3ed859123
ellilglor <thomassmekens14@gmail.com>
2025-01-23 11:41:58 +0100

    Fixed new method cyclomatic complexity

--- Src/Extensions/StringExtensions.cs
+++ Src/Extensions/StringExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Kozma.net.Src.Enums;
 using System.Globalization;
+using System.Text;
 using System.Text.RegularExpressions;
 
 namespace Kozma.net.Src.Extensions;
@@ -77,45 +78,19 @@ public static partial class StringExtensions
             i = 0;
         var src = input.ToCharArray();
         bool skip = false;
-        char ch;
+
         for (; i < len; i++)
         {
-            ch = src[i];
-            switch (ch)
+            var ch = src[i];
+            if (char.IsWhiteSpace(ch) && !skip)
             {
-                case '\u0020':
-                case '\u00A0':
-                case '\u1680':
-                case '\u2000':
-                case '\u2001':
-                case '\u2002':
-                case '\u2003':
-                case '\u2004':
-                case '\u2005':
-                case '\u2006':
-                case '\u2007':
-                case '\u2008':
-                case '\u2009':
-                case '\u200A':
-                case '\u202F':
-                case '\u205F':
-                case '\u3000':
-                case '\u2028':
-                case '\u2029':
-                case '\u0009':
-                case '\u000A':
-                case '\u000B':
-                case '\u000C':
-                case '\u000D':
-                case '\u0085':
-                    if (skip) continue;
-                    src[index++] = ch;
-                    skip = true;
-                    continue;
-                default:
-                    skip = false;
-                    src[index++] = ch;
-                    continue;
+                src[index++] = ' ';
+                skip = true;
+            }
+            else
+            {
+                skip = false;
+                src[index++] = ch;
             }
         }
 
